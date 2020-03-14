5fdd2c62e08aeffd95d77af31765e01f68ff4a9b
praveencts <gspraveenkumaraws@gmail.com>
2020-03-14 10:10:04 +0530

    new suite file

---
+++ TestSuite.java
@@ -0,0 +1,14 @@
+package examples1;
+
+import org.junit.jupiter.api.*;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("examples1")
+public class TestSuite {
+  //nothing
+}
